9b4b5d7d2288c161eab1ccbdde8d55d524b3c200
Jinhyuk Lee <jin@zaricorp.com>
2017-10-01 22:10:17 +0900

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,98 @@
 const queues = require('./lib/queue')
 
 module.exports = (grunt) => {
-  grunt.registerTask('start_find_reservation', (company_id = 0) => {
-    const queue_5m = queues.get('queue_5m')
-    queue_5m.create('find reservation', {
-      company_id: company_id
-    }).priority('high').save(err => {
-      console.log(err)
-    })
+  grunt.registerTask('stop', '...', async function (company_id = 0) {
+    done = this.async()
+    try {
+      const queue = queues.get('queue_5m_bull')
+      console.log('> JobCounts', await queue.getJobCounts())
+
+      const ID = await queues.job('#find-reservation-'+company_id).data('original_id')
+
+      console.log('FOUND?', ID)
+      let job = await queue.getJob(ID)
+      if(job){
+        console.log('FOUND!!')
+        await job.remove()
+      }
+      console.log('> JobCounts', await queue.getJobCounts())
+      
+      done()
+    } catch (err) {
+      done(err)
+    }
+  })
+
+  grunt.registerTask('start', '...', async function (company_id = 0) {
+    done = this.async()
+    try {
+      const queue = queues.get('queue_5m_bull')
+      console.log('> JobCounts', await queue.getJobCounts())
+
+      const ID = `#find-reservation-${company_id}`
+
+      job = await queue.add('find reservation', {company_id: company_id}, {
+        delay: 1000,
+        attempts: 10,
+        backoff: {
+          // type: 'exponential',
+          type: 'fixed',
+          delay: 1000,
+        },
+        jobId: ID,
+        repeat: {
+          cron: '*/1 * * * *',
+        },
+        timeout: 5000,
+        removeOnComplete: false,
+        removeOnFail: false,
+      })
+
+      if(job){
+        queues
+          .job(ID)
+          .data('original_id', job.id)
+      }
+
+      
+      // console.log('>>>', (job && job.id))
+      console.log('> JobCounts', await queue.getJobCounts())
+      
+      done()
+    } catch (err) {
+      done(err)
+    }
+    
+    // queue.getJobCounts().then( r => {
+    //   console.log('> JobCounts', r)
+    // })
+    // console.log(2)
+    // // queue_5m.create('find reservation', {
+    // //   company_id: company_id
+    // // }).priority('high').save(err => {
+    // //   console.log(err)
+    // // })
+    // queue.add('find reservation', {company_id: company_id}, {
+    //   delay: 1000,
+    //   attempts: 10,
+    //   backoff: {
+    //     // type: 'exponential',
+    //     type: 'fixed',
+    //     delay: 1000,
+    //   },
+    //   jobId: `#find-reservation-1`,
+    //   // repeat: {
+    //   //   cron: '*/1 * * * *',
+    //   // },
+    //   timeout: 5000,
+    //   removeOnComplete: false,
+    //   removeOnFail: false,
+    // })
+    // console.log(3)
+
+    // console.log('> JobCounts', await queue.getJobCounts())
+    // console.log(4)
+    
+    // done('dfgdfg')
   })
 }
--- index.js
+++ index.js
@@ -14,27 +14,96 @@ queue_5m
   .on('error', (err) => {
     console.error(err)
   })
+  
+  queue_5m.watchStuckJobs(1000)
+  
+  queue_5m.process('find reservation', (job, done) => {
+    console.log('job.data', job.data)
+    setTimeout( () => {
+      // do something
+      if(Math.random()*100 > 50){
+        process.exit(-1)
+      }
+      queue_5m.create('find reservation', {
+        company_id: job.data.company_id
+      }).delay(1000).save()
+      done()
+    }, 2000)
+  })
+//   queue_5m.create('find reservation', {
+//     company_id: 1
+//   }).delay(1000).save(err => {
+//     if(err) console.log(err)
+//     })
+// console.log(chalk`> {green NOW RUNNING}`)
 
-queue_5m.watchStuckJobs(1000)
 
-queue_5m.process('find reservation', (job, done) => {
-  console.log('job.data', job.data )
+const queue_5m_bull = queues.get('queue_5m_bull')
+// queue_5m_bull.clean(1, 'delayed')
+// queue_5m_bull.clean(1)
+queue_5m_bull.on('completed', (job, result) => {
+  console.log('complete', job.id, result)
+})
+queue_5m_bull.on('error', error => {
+  console.log('> error', error)
+})
+
+queue_5m_bull.process(async (job, done) => {
+  console.log('>>>>skip')
+  done()
+})
+queue_5m_bull.process('find reservation', 2, async (job, done) => {
+  console.log('> JobCounts', await queue_5m_bull.getJobCounts())
+
+  console.log('job.attemptsMade', job.attemptsMade)
+  console.log('job.data', job.data)
+  console.log('job.id', job.id)
+  // return done(new Error('ssdf'))
+  // throw new Error('skip')
+  console.log('job.data ?')
+
   setTimeout( () => {
     // do something
+    // return done(new Error('sdfsdfdsf'))
     if(Math.random()*100 > 50){
-      process.exit(-1)
+      // throw new Error('sdfdsf')
+      return done(new Error('sdfsdfdsf'))
     }
-    queue_5m.create('find reservation', {
-      company_id: job.data.company_id
-    }).delay(1000).save()
-    done()
+    done(null, {status: 200})
   }, 2000)
 })
 
-console.log(chalk`> {green NOW RUNNING}`)
+// queue_5m_bull.add('find reservation', {company_id: 1}, {
+//   delay: 1000,
+//   attempts: 20,
+//   backoff: {
+//     // type: 'exponential',
+//     type: 'fixed',
+//     delay: 1000,
+//   },
+//   jobId: `#find-reservation-1`,
+//   timeout: 5000,
+//   removeOnComplete: false,
+//   removeOnFail: false,
+// })
 
-queue_5m.create('find reservation', {
-  company_id: 1
-}).delay(1000).save(err => {
-  if(err) console.log(err)
-})
+async function main() {
+  console.log(chalk`> {green NOW RUNNING}`)
+  console.log('> JobCounts', await queue_5m_bull.getJobCounts())
+
+  setInterval( async function() {
+    const queue = queues.get('queue_5m_bull')
+
+    // let ID = 'repeat:find reservation:#find-reservation-3:1506840960000'
+    console.log(await queues.job('#find-reservation-5').data('original_id'))
+    return 
+    let job = await queue.getJob(ID)
+    // console.log(job)
+    if(job){
+      console.log(chalk`> ${ID}: `, job.data)
+    }else{      
+      console.log(chalk`> ${ID}: {blue null}`)
+    }
+  }, 1000)
+}
+main()
